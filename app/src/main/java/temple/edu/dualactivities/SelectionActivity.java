242715423ef9994ad659a38f35af988c9519d730
BrockMorris42611 <brock36morris@gmail.com>
2021-03-08 21:01:58 -0500

    changed the setOnItemSelectedListener() to setOnItemClickListener() since we are no longer using a spinner. this also means i got rid of the check to make sure we arent selecting the "choose" option and that we are always returning to an unselected spinner. I also got rid of the getDropDownView() from the adapter because it wont be used since wer are in a GridViewLayout now. the size finder for the source of the adapter has also been changed to no longer be size-1 because we took out the "choose" option. I added more cats too.

--- app/src/main/java/temple/edu/dualactivities/SelectionActivity.java
+++ app/src/main/java/temple/edu/dualactivities/SelectionActivity.java
@@ -44,40 +44,31 @@ public class SelectionActivity extends AppCompatActivity {
         instructions = findViewById(R.id.instruct_text_view);
 
         instructions.setText(R.string.instructions);
+        instructions.setTextSize(20);
 
 
         ArrayList<Image> listToAdapter = new ArrayList<Image>(); //Create the array list to hold all the Image objects
         //In this part below i am setting up a new Image object for each image
         listToAdapter.add(new Image("black kit", R.drawable.black_kit));
         listToAdapter.add(new Image("gray kit", R.drawable.gray_kit));
-        listToAdapter.add(new Image("red kit", R.drawable.red_kit));
+        listToAdapter.add(new Image("red kits", R.drawable.red_kit));
         listToAdapter.add(new Image("tigre", R.drawable.tigre));
         listToAdapter.add(new Image("white kit", R.drawable.white_kit));
+        listToAdapter.add(new Image("cup kits", R.drawable.cup_kits));
 
         AdapterForCats = new ImageAdapter(this ,listToAdapter); // set up the new adapter
 
         menu.setAdapter(AdapterForCats); // PASS THE ADAPTER THE DATA SET WE MADE ABOVE
 
-        menu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        menu.setOnItemClickListener((parent, view, position, id) -> { //setOnItemClickListener is for the grid view as opp to spinner
 
-                if(position == 0) //i dont want to switch views if this is the main selector one for letting them know what to do
-                    return;
+            int passableImage = ((Image)parent.getItemAtPosition(position)).getPicture(); //get the immage at the position ready as an int
+            String passableDescription = ((Image)parent.getItemAtPosition(position)).getName();
 
-                int passableImage = ((Image)parent.getItemAtPosition(position)).getPicture(); //get the immage at the position ready as an int
-                String passableDescription = ((Image)parent.getItemAtPosition(position)).getName();
-
-                Intent intent = new Intent(SelectionActivity.this, DisplayActivity.class); //set up the intent
-                intent.putExtra("image", passableImage); //add image to intent
-                intent.putExtra("description", passableDescription);
-                startActivity(intent); //start the new activity
-                menu.setSelection(0); // to reset it upon returning
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                parent.getSelectedView();
-            }
+            Intent intent = new Intent(SelectionActivity.this, DisplayActivity.class); //set up the intent
+            intent.putExtra("image", passableImage); //add image to intent
+            intent.putExtra("description", passableDescription);
+            startActivity(intent); //start the new activity
         });
     }
 }
