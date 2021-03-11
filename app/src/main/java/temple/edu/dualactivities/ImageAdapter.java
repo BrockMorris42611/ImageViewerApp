b0124f83e977164b0b27c1258e11b7e56b3da092
BrockMorris42611 <brock36morris@gmail.com>
2021-03-11 10:49:46 -0500

    I am having super weird git issues that might corrupt some files. please let me know but they should be smoothened out. this is all weird to me bc i dony know why branching wasnt working right

--- app/src/main/java/temple/edu/dualactivities/ImageAdapter.java
+++ app/src/main/java/temple/edu/dualactivities/ImageAdapter.java
@@ -1,6 +1,7 @@
 package temple.edu.dualactivities;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,89 +12,80 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import static android.graphics.Color.BLACK;
 import static android.graphics.Color.CYAN;
+import static android.graphics.Color.GRAY;
 import static android.graphics.Color.RED;
 import static android.graphics.Color.WHITE;
+import static android.view.View.TEXT_ALIGNMENT_CENTER;
+import static android.widget.TextView.AUTO_SIZE_TEXT_TYPE_NONE;
 
 public class ImageAdapter extends BaseAdapter {
 
-        Context context;
-        ArrayList<Image> Cat_List; // this is the array list we made for our data set
+    Context context;
+    ArrayList<Image> Cat_List; // this is the array list we made for our data set
 
-        ImageAdapter(Context context, ArrayList<Image> Cat_List){
-            this.context = context;
-            this.Cat_List = Cat_List;
+    ImageAdapter(Context context, ArrayList<Image> Cat_List){
+        this.context = context;
+        this.Cat_List = Cat_List;
+    }
+    @Override
+    public int getCount() {
+        return Cat_List.size();
+    } // get the size of the array list
+    @Override
+    public Object getItem(int position) {
+        return Cat_List.get(position);
+    } // simply return the item at the specific position in the array list but return the name of that item
+    @Override
+    public long getItemId(int position) { return 0; }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        TextView nameTextView; LinearLayout catLinearLayout; ImageView picture_of_desc;// this is the view to be displayed at the when the drop down menu isnt clicked or basically when something has been clicked and is set to be seen
+
+
+        if(convertView == null){ //first use and not yet recycled
+            catLinearLayout = new LinearLayout(context); // set up the layout based on the context
+            catLinearLayout.setOrientation(LinearLayout.VERTICAL); // give it the vertical layout to appear like a standard list
+            if(position%2==0)
+                catLinearLayout.setBackgroundColor(GRAY); //stand out with that nice cyan blue
+            else
+                catLinearLayout.setBackgroundColor(RED);
+
+            picture_of_desc = new ImageView(context);
+            picture_of_desc.setAdjustViewBounds(true);
+            picture_of_desc.setMaxHeight(300);
+            picture_of_desc.setMaxWidth(225);
+
+            nameTextView = new TextView(context); // hold the given name of the cat or the item that we select
+            nameTextView.setTextSize(20);
+
+
+            catLinearLayout.addView(picture_of_desc);
+            catLinearLayout.addView(nameTextView); // add the textView to the layout to be displayed
+
+        }else{
+            catLinearLayout = (LinearLayout) convertView; // this is for when we are recycling to save resources
+            nameTextView = (TextView) catLinearLayout.getChildAt(1); // since layouts are basically singular trees with the views as the nodes we do this
+            picture_of_desc = (ImageView) catLinearLayout.getChildAt(0);
         }
-        @Override
-        public int getCount() {
-            return Cat_List.size()-1;
-        } // get the size of the array list
-        @Override
-        public Object getItem(int position) {
-            return Cat_List.get(position);
-        } // simply return the item at the specific position in the array list but return the name of that item
-        @Override
-        public long getItemId(int position) { return 0; }
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
 
-            TextView nameTextView; LinearLayout catLinearLayout; // this is the view to be displayed at the when the drop down menu isnt clicked or basically when something has been clicked and is set to be seen
+        picture_of_desc.setImageResource(Cat_List.get(position).getPicture());
 
-            if(convertView == null){ //first use and not yet recycled
-                catLinearLayout = new LinearLayout(context); // set up the layout based on the context
-                catLinearLayout.setOrientation(LinearLayout.VERTICAL); // give it the vertical layout to appear like a standard list
-                catLinearLayout.setBackgroundColor(CYAN); //stand out with that nice cyan blue
-                nameTextView = new TextView(context); // hold the given name of the cat or the item that we select
-                catLinearLayout.addView(nameTextView); // add the textView to the layout to be displayed
-            }else{
-                catLinearLayout = (LinearLayout) convertView; // this si for when we are recycling to save resources
-                nameTextView = (TextView) catLinearLayout.getChildAt(0); // since layouts are basically singular trees with the views as the nodes we do this
-            }
+        nameTextView.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        nameTextView.setText(Cat_List.get(position).getName()); //set the textView to display correct cat
+        nameTextView.setY((picture_of_desc.getMaxHeight()/-2)+nameTextView.getMaxHeight()); //trying to just make the y-position the same as half the height of the picture plus the max heioght of the TextView
+        if(nameTextView.getText().equals("black kit"))
+            nameTextView.setTextColor(CYAN);
+        else
+            nameTextView.setTextColor(BLACK);
 
 
-            nameTextView.setGravity(Gravity.CENTER_HORIZONTAL);
-            nameTextView.setText(Cat_List.get(position).getName()); //set the textView to display correct cat
 
-            return catLinearLayout; //return the layout
-        }
-        @Override
-        public View getDropDownView(int position, View convertView, ViewGroup parent) { // this is used when we actually drop down the menu with the default arrow click
-
-            TextView nameTextView; // this drop down is doing to store an image(ImageView) to the right of the name of the picture(TextView)
-            LinearLayout catLinearLayout;
-            ImageView cat_pic_holder;
-
-            if (convertView == null) { //first use and not yet recycled
-                catLinearLayout = new LinearLayout(context); //set up the view linearly as we did above but now give a red color to distinguish visibly
-                catLinearLayout.setOrientation(LinearLayout.VERTICAL);
-
-                if (position % 2 == 0) //BARBER POLE CAT SCREEN
-                    catLinearLayout.setBackgroundColor(RED);
-                else
-                    catLinearLayout.setBackgroundColor(WHITE);
-
-                nameTextView = new TextView(context); //get the new name and picture views set up
-                cat_pic_holder = new ImageView(context);
-
-                cat_pic_holder.setAdjustViewBounds(true); // need to use this to
-
-                cat_pic_holder.setMaxHeight(100);
-
-                catLinearLayout.addView(nameTextView); //add the two new vies to the new layout
-                catLinearLayout.addView(cat_pic_holder);
-            } else {
-                catLinearLayout = (LinearLayout) convertView; // do this when the view is ready to be recycled
-                nameTextView = (TextView) catLinearLayout.getChildAt(0);
-                cat_pic_holder = (ImageView) catLinearLayout.getChildAt(1);
-            }
-            if(position == 0) // dont show the chooser option
-                catLinearLayout.setVisibility(View.INVISIBLE);
-            else {
-                nameTextView.setText(Cat_List.get(position).getName()); //post the name
-                cat_pic_holder.setImageResource(Cat_List.get(position).getPicture()); //post the picture
-            }
-            return catLinearLayout; //return the dropdown
-        }
+        return catLinearLayout; //return the layout
+    }
 }
 class Image{ //just to make having one array easier if not just eliminating need for 2 arrays which could become asymmetrical
     private final String name;
@@ -104,8 +96,8 @@ class Image{ //just to make having one array easier if not just eliminating need
         this.picture = picture;
     }
     public String getName() {
-            return name;
-        }public int getPicture() {
-            return picture;
-        }
+        return name;
+    }public int getPicture() {
+        return picture;
+    }
 }
--- app/src/main/java/temple/edu/dualactivities/SelectionActivity.java
+++ app/src/main/java/temple/edu/dualactivities/SelectionActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -27,7 +28,8 @@ public class SelectionActivity extends AppCompatActivity {
 
     ImageAdapter AdapterForCats;
     boolean touched = false;
-    Spinner menu;
+    GridView menu;
+    TextView instructions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,41 +37,38 @@ public class SelectionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle(R.string.activity_one_name);
 
-        menu = findViewById(R.id.menu_spinner);
+        menu = findViewById(R.id.menu_gridView);
+
+        menu.setNumColumns(3);
+
+        instructions = findViewById(R.id.instruct_text_view);
+
+        instructions.setText(R.string.instructions);
+        instructions.setTextSize(20);
+
 
         ArrayList<Image> listToAdapter = new ArrayList<Image>(); //Create the array list to hold all the Image objects
         //In this part below i am setting up a new Image object for each image
-        listToAdapter.add(new Image("Please select the cat of your choosing", 0));
         listToAdapter.add(new Image("black kit", R.drawable.black_kit));
         listToAdapter.add(new Image("gray kit", R.drawable.gray_kit));
-        listToAdapter.add(new Image("red kit", R.drawable.red_kit));
+        listToAdapter.add(new Image("red kits", R.drawable.red_kit));
         listToAdapter.add(new Image("tigre", R.drawable.tigre));
         listToAdapter.add(new Image("white kit", R.drawable.white_kit));
+        listToAdapter.add(new Image("cup kits", R.drawable.cup_kits));
 
         AdapterForCats = new ImageAdapter(this ,listToAdapter); // set up the new adapter
 
         menu.setAdapter(AdapterForCats); // PASS THE ADAPTER THE DATA SET WE MADE ABOVE
 
-        menu.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        menu.setOnItemClickListener((parent, view, position, id) -> { //setOnItemClickListener is for the grid view as opp to spinner
 
-                if(position == 0) //i dont want to switch views if this is the main selector one for letting them know what to do
-                    return;
+            int passableImage = ((Image)parent.getItemAtPosition(position)).getPicture(); //get the immage at the position ready as an int
+            String passableDescription = ((Image)parent.getItemAtPosition(position)).getName();
 
-                int passableImage = ((Image)parent.getItemAtPosition(position)).getPicture(); //get the immage at the position ready as an int
-                String passableDescription = ((Image)parent.getItemAtPosition(position)).getName();
-
-                Intent intent = new Intent(SelectionActivity.this, DisplayActivity.class); //set up the intent
-                intent.putExtra("image", passableImage); //add image to intent
-                intent.putExtra("description", passableDescription);
-                startActivity(intent); //start the new activity
-                menu.setSelection(0); // to reset it upon returning
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                parent.getSelectedView();
-            }
+            Intent intent = new Intent(SelectionActivity.this, DisplayActivity.class); //set up the intent
+            intent.putExtra("image", passableImage); //add image to intent
+            intent.putExtra("description", passableDescription);
+            startActivity(intent); //start the new activity
         });
     }
 }
